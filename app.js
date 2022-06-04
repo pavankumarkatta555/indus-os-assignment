57c1399fe49ece46d43d860d3b222f6209c82218
Pavan Kumar Katta <pavankumarkatta555@gmail.com>
2022-06-04 12:23:01 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "savedtexts.db");
+
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/savedtexts", async (request, response) => {
+  const getTodoQuery = `
+    SELECT
+      *
+    FROM
+      savedtexts`;
+  const todo = await database.all(getTodoQuery);
+  response.send(todo);
+});
+
+app.post("/", async (request, response) => {
+  const { savedtxt } = request.body;
+
+  const postTodoQuery = `
+  INSERT INTO
+    savedtexts(savedtxt)
+  VALUES
+    ('${savedtxt}')`;
+
+  console.log(savedtxt);
+
+  await database.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+module.exports = app;
